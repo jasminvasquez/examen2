8ddcd014ebeae74cc6ab52aaf5fffaff8d90a1f4
jasminvasquez <deysijasmin@unitec.com>
2014-03-18 23:47:34 -0600

    Update exam2.java

--- src/exam2.java
+++ src/exam2.java
@@ -4,48 +4,37 @@ import java.util.Scanner;
 public class exam2 {
 
 	public static void main(String[] args) {
-	Scanner  mi_scanner=new Scanner(System.in);
-	        int nota1,nota2,nota3;
-	        System.out.print("alumno1:");
-	        System.out.print("Ingrese primer nota:");
-	        nota1= mi_scanner.nextInt();
-	        System.out.print("Ingrese segunda nota:");
-	        nota2= mi_scanner.nextInt();
-	        System.out.print("Ingrese tercer nota:");
-	        nota3= mi_scanner.nextInt();
-	        int promedio=(nota1 + nota2 + nota3) / 3;
-	        if (promedio>=60) {
-	            System.out.print("aprobado");    
-	        } else 
-	                System.out.print("Reprobado");
-	                System.out.print("alumno2:");
-	    	        System.out.print("Ingrese primer nota:");
-	    	        nota1= mi_scanner.nextInt();
-	    	        System.out.print("Ingrese segunda nota:");
-	    	        nota2= mi_scanner.nextInt();
-	    	        System.out.print("Ingrese tercer nota:");
-	    	        nota3= mi_scanner.nextInt();
-	    	        int promedio1=(nota1 + nota2 + nota3) / 3;
-	    	        if (promedio1>=60) {
-	    	            System.out.print("aprobado");    
-	    	        } else 
-	    	           
-	    	                System.out.print("Reprobado");          
-	    	                System.out.print("alumno3:");
-	    	    	        System.out.print("Ingrese primer nota:");
-	    	    	        nota1= mi_scanner.nextInt();
-	    	    	        System.out.print("Ingrese segunda nota:");
-	    	    	        nota2= mi_scanner.nextInt();
-	    	    	        System.out.print("Ingrese tercer nota:");
-	    	    	        nota3= mi_scanner.nextInt();
-	    	    	        int promedio2=(nota1 + nota2 + nota3) / 3;
-	    	    	        if (promedio2>=60) {
-	    	    	            System.out.print("aprobado");    
-	    	    	        } else 
-	    	    	           
-	    	    	                System.out.print("Reprobado");          
-	    	                
-	            }
-	        }
-
+        int num1,num2,suma,resta,multi,division,n;
+		Scanner mi_escaner = new Scanner (System.in);
+		System.out.print("Ingrese primer numero:"); 
+		num1=mi_escaner.nextInt();
+		System.out.print("Ingrese segundo numero:"); 
+		num2=mi_escaner.nextInt();
+		System.out.println("Mostrar Opcion:"); 
+		System.out.println("1:"); 
+		System.out.println("2:"); 
+		System.out.println("3:"); 
+		System.out.println("4:"); 
+		n=mi_escaner.nextInt(); 
+		switch (n){ 
+		case 1:
+			suma=num1+num2; 
+			System.out.println("La suma es"+suma); 
+			break; 
+			case 2: 
+				resta=num1-num2;
+				System.out.println("La resta es"+resta);
+            break; 
+            case 3:
+            	multi=num1*num2; 
+            System.out.println("La multiplicacion es"+multi); 
+            break; 
+            case 4: 
+            	division=num1/num2; 
+            	System.out.println("La division es"+division);
+            	break; 
+            
+            	} 
+		} 
+	} 
 	
